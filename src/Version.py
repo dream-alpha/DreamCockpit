f10f7fc2de654ea8f99ff2b71716271974e7417b
root <swmaniacster@gmail.com>
2025-10-19 17:05:40 +0200

    0.3.6

--- src/Version.py
+++ src/Version.py
@@ -21,6 +21,6 @@
 
 PLUGIN = "DreamCockpit"
 ID = "CPT"
-VERSION = "0.3.5"
+VERSION = "0.3.6"
 COPYRIGHT = "2018-2025 by dream-alpha"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
